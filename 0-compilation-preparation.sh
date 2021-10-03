ea1066933e3ad6df6e227db383635b3a00e48654
lilo <lilo@lilo-co.almostmy.com>
2021-10-03 23:06:26 +0200

    add mixing checkout commands

--- 0-compilation-preparation.sh
+++ 0-compilation-preparation.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/batocera-linux/batocera.linux.git
+git submodule init
+git submodule update
 # Currently this is done mainly to customise shaders used for scanlines for ones the CHA can handle
 cp -rfv ./batocera.linux-dir-customisation/* ./batocera.linux/
